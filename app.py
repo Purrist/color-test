189a71dc3553976aa814bf4761b76ffb85e0b098
purriste <2633705241@qq.com>
2025-07-30 21:04:45 +0800

    1.4（优化手机ui）

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,8 @@ app = Flask(__name__)
 def stroop_test():
     return render_template('index.html')
 
-# 这一部分对于Zeabur的自动检测很重要
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # 监听 0.0.0.0 以允许局域网访问
+    # Zeabur 等部署平台会自动忽略这一部分，所以不会影响云端部署
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
